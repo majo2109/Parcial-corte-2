fd556339ac2ffe963b81ce5c9f4709345a582156
majo2109 <majorincon2109@gmail.com>
2025-10-28 10:03:59 -0500

    nuevo archivo models

--- models.py
+++ models.py
@@ -1,25 +1,53 @@
-from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, List
+from sqlmodel import SQLModel, Field, Relationship
 
-class Matricula(SQLModel, table=True):
+
+class EstudianteBase(SQLModel):
+    nombre: str
+    codigo: str
+    semestre: int
+
+class CursoBase(SQLModel):
+    nombre: str
+    codigo: str
+    creditos: int
+
+class MatriculaBase(SQLModel):
+    estudiante_id: int
+    curso_id: int
+
+class Estudiante(EstudianteBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    matriculas: List["Matricula"] = Relationship(back_populates="estudiante")
+
+class Curso(CursoBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    matriculas: List["Matricula"] = Relationship(back_populates="curso")
+
+class Matricula(MatriculaBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     estudiante_id: int = Field(foreign_key="estudiante.id")
     curso_id: int = Field(foreign_key="curso.id")
-    
-    estudiante: "Estudiante" = Relationship(back_populates="matriculas")
-    curso: "Curso" = Relationship(back_populates="matriculas")
 
-class Estudiante(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    nombre: str
-    email: str
-    semestre: int 
-    cursos: List["Curso"] = Relationship(back_populates="estudiantes", link_model=Matricula)
+    estudiante: Optional[Estudiante] = Relationship(back_populates="matriculas")
+    curso: Optional[Curso] = Relationship(back_populates="matriculas")
 
-class Curso(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    nombre: str
-    creditos: int
-    horario: str
-    estudiantes: List[Estudiante] = Relationship(back_populates="cursos", link_model=Matricula)
+class EstudianteCreate(EstudianteBase):
+    pass
+
+class EstudianteRead(EstudianteBase):
+    id: int
+
+class CursoCreate(CursoBase):
+    pass
+
+class CursoRead(CursoBase):
+    id: int
+
+class MatriculaCreate(MatriculaBase):
+    pass
+
+class MatriculaRead(MatriculaBase):
+    id: int
+
     
