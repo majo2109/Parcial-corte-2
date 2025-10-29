5f6489c051f6e346e472d8bcd02af350d0410150
majo2109 <majorincon2109@gmail.com>
2025-10-29 09:42:26 -0500

    correcion del main

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
-from db import init_db
+from db import create_tables
 from routers import estudiantes, cursos, matriculas
 
-app = FastAPI(title="Proyecto FastAPI + SQLModel")
+def create_tables_lifespan():
+    
+    print("Iniciando aplicación: Creando base de datos y tablas...")
+    create_tables() 
+    
+    yield 
+    print("Apagando aplicación: Limpieza de recursos completada.")
 
-@app.on_event("startup")
-def on_startup():
-    init_db()
+app = FastAPI(
+    lifespan=create_tables_lifespan, 
+    title="Sistema de Gestión Universitaria"
+)
 
-app.include_router(estudiantes.router)
-app.include_router(cursos.router)
-app.include_router(matriculas.router)
+app.include_router(estudiantes.router, tags=["Estudiantes"], prefix="/estudiantes")
+app.include_router(cursos.router, tags=["Cursos"], prefix="/cursos")
+app.include_router(matriculas.router, tags=["Matrículas"], prefix="/matriculas")
 
 @app.get("/")
-def root():
-    return {"mensaje": "Bienvenido al sistema de gestión de cursos y estudiantes"}
+async def root():
+    return {"message": "Bienvenido al Sistema de Gestión Universitaria"}
+
